7f086e001e38460b89dd5eedc40912d93cb8ecf9
Rozdymkacz <szymon.dziurowicz@gmail.com>
2026-06-08 14:36:51 +0200

    feat: Enhance USB OTG HS support with DMA and memory protection configuration

--- CM7/Core/Inc/tusb_config.h
+++ CM7/Core/Inc/tusb_config.h
@@ -11,6 +11,8 @@ extern "C" {
 // USB HS + external ULPI PHY (DWC2)
 #define CFG_TUSB_RHPORT1_MODE     (OPT_MODE_DEVICE | OPT_MODE_HIGH_SPEED)
 
+// TinyUSB DWC2 driver
+#define CFG_TUD_DWC2_DMA_ENABLE 1
 
 // Endpoint 0
 #define CFG_TUD_ENDPOINT0_SIZE    64
@@ -24,6 +26,8 @@ extern "C" {
 #define CFG_TUD_CDC_TX_BUFSIZE 512
 
 #define CFG_TUD_CDC_EP_BUFSIZE 512
+#define CFG_TUSB_MEM_SECTION __attribute__((section(".SRAM1")))
+#define CFG_TUSB_MEM_ALIGN __attribute__((aligned(32)))
 
 // Disable unused classes
 #define CFG_TUD_MSC               0
@@ -32,7 +36,7 @@ extern "C" {
 #define CFG_TUD_VENDOR            0
 
 // Debug
-#define CFG_TUSB_DEBUG            0
+#define CFG_TUSB_DEBUG            3
 
 #ifdef __cplusplus
 }
